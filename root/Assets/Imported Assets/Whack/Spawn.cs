7787a551946b3b47199127cb191fc581823ff899
chuwilliamson <chuwilliamson@gmail.com>
2015-05-12 18:49:44 -0500

    organized project

---
+++ root/Assets/Imported Assets/Whack/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour 
+{
+	public GameObject demonDoll;
+    public GameObject teddybear;
+	float moveSpeed = 5.0f;
+
+	void Start()
+	{
+      
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.Translate (Vector3.up * Time.deltaTime * moveSpeed);
+		//transform.Translate (0, 0, Time.deltaTime * 1);
+	}
+
+}
