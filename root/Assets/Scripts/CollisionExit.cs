bf630322c979c4fe14eb2522698a3b2e88aec91f
Andrew Smith <badandyssj@gmail.com>
2015-05-13 13:09:48 -0500

    Merge remote-tracking branch 'chuwilliamson/master'
    Conflicts:
    	root/Assets/Other/Prefabs/Sphere.prefab

---
+++ root/Assets/Scripts/CollisionExit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionExit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    
+    /// <summary>
+    /// Precondition: object that this script is attached to must have a rigid body and a collider 
+    /// Postcondition: an object must touch this object
+    /// Description: when this object is  touched we will transition to a new scene given by the _lvlname parameter
+    /// </summary>
+    /// <param name="col"></param>
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("Win");
+            Application.LoadLevel(_lvlName);
+        }
+    }
+
+    /// <summary>
+    /// this is the name of the level that we want to transition to 
+    /// this value must b3e set in the inspector
+    /// </summary>
+    public string _lvlName;
+    //public Event d = new Event();
+}
