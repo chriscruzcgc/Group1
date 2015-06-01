d8134bb2618bd0890b6699f0c72754d28742484b
chuwilliamson <chuwilliamson@gmail.com>
2015-06-01 15:30:09 -0500

    lvl loader comments
    added comments for usage

--- root/Assets/Scripts/UIManagerScript.cs
+++ root/Assets/Scripts/UIManagerScript.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour 
 {
+    /// <summary>
+    /// helper function that will be attached to a go in the scene
+    /// reference the public function in the OnClick eventlist
+    /// </summary>
+    /// <param name="lvl">
+    /// the lvl to be loaded
+    /// </param>
     public void LoadLevel(string lvl)
     {
         try
