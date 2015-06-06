c85eaf37fffb2e4c28988f70b0192d4f36d7b544
Matthew Williamson <chuwilliamson@gmail.com>
2015-06-05 19:11:33 -0500

    map changes and lvl loading

---
+++ root/Assets/Scripts/MapLayout.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapLayout : MonoBehaviour {
+    public void Transition(string lvl)
+    {
+        Application.LoadLevel(lvl);
+    }
+
+
+
+    /// <summary>
+    /// gimme the wings?
+    /// if wings is -1 default
+    /// else make some states and button behavior
+    /// </summary>
+    /// <returns></returns>
+    public int CheckGameState()
+    {
+        int currentState = -1;
+        switch (currentState)
+        {
+            case 0:
+                //lock all the buttons but falling
+                break;
+            case 1:
+                //through 4
+                break;
+
+        }
+        return -1;
+    }
+
+}
