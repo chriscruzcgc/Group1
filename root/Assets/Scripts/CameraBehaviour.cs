af3fa2a0083fc643e1b2c585f44e97e8046a6082
TamC173 <tchan173@yahoo.com>
2015-06-08 13:36:58 -0500

    changes matthew made
    map camera behavior

---
+++ root/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+    public bool cam = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ZoomCamera(cam);
+        }
+    }
+    public void ZoomCamera(bool state)
+    {
+        Camera.main.GetComponent<Animator>().SetBool("in",state);
+    }
+
+}
